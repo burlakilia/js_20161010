76617de3b92ea2d239b0f0c85179ea878745b34d
Anton <lascivolin@gmail.com>
2016-12-01 21:52:09 +0300

    add form menu

---
+++ blocks/menuForm/menuForm.js
@@ -0,0 +1,39 @@
+
+'use strict';
+
+(() => {
+    
+    // import fest.MenuFormTemplate
+    
+    class MenuForm {
+        
+        constructor(node, menu) {
+            
+            this.menu= menu;
+            this.node = this.render(node);
+            
+            this.formButton = document.querySelector('.menu__form_submit');
+            this.formTitle = document.querySelector('.menu__form_title');
+            this.formHref = document.querySelector('.menu__form_href');
+            
+            this.onButtonSubmit = this.onButtonSubmit.bind(this);
+            this.formButton.addEventListener('click', this.onButtonSubmit);
+        }
+        
+        render(node) {
+            node.innerHTML = MenuFormTemplate();
+            return node;
+        }
+        
+        onButtonSubmit() {
+            const options = {
+                title: this.formTitle.value,
+                href: this.formHref.value
+            };
+            this.menu.add(options);
+        }
+        
+    }
+    
+    window.MenuForm = MenuForm;
+})();
